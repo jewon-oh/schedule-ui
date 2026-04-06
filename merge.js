ea4e0c6e7e308ccd37cdbf6447c7ea6618756017
jewon <96jwjw@naver.com>
2026-04-06 16:05:29 +0900

    build: 단일 파일 패키징을 위한 자동 병합 스크립트(merge.js) 추가

---
+++ merge.js
@@ -0,0 +1,28 @@
+﻿const fs = require('fs');
+
+let scheduleJs = fs.readFileSync('schedule-ui.tmp.js', 'utf8');
+let timerJs = fs.readFileSync('timer-ui.js', 'utf8');
+
+// 1. Remove import from schedule
+scheduleJs = scheduleJs.replace('import "./timer-ui.js";\n', '');
+scheduleJs = scheduleJs.replace('import "./timer-ui.js";\r\n', '');
+scheduleJs = scheduleJs.replace('import "./timer-ui.js";', '');
+
+// 2. Remove imports from timer
+const timerLines = timerJs.split('\n');
+let newTimer = [];
+let skipImport = true;
+for (let line of timerLines) {
+  if (skipImport) {
+    if (line.includes('} from "https://cdn.jsdelivr.net/gh/lit')) {
+      skipImport = false;
+    }
+  } else {
+    newTimer.push(line);
+  }
+}
+timerJs = newTimer.join('\n');
+
+// 3. Append
+fs.writeFileSync('schedule-ui.js', scheduleJs + '\n\n' + timerJs, 'utf8');
+console.log("Merged safely");
